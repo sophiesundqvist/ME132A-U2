9621eb9e9a330e77caf665e77836167304b25b15
sophiesundqvist <sophie.sundqvist@hotmail.com>
2022-02-25 13:32:06 +0100

    added objects to database

--- database.js
+++ database.js
@@ -1,1 +1,98 @@
 "use strict"
+
+let database = [
+    {
+        id: 1,
+        destination: "Paris",
+        country: "Frankrike",
+        type: "Cityresa",
+        grade: 5,
+        covidRestrictions: true  
+    },
+
+    {
+        id: 2,
+        destination: "Mallorca",
+        country: "Spanien",
+        type: "Solsemester",
+        grade: 4,
+        covidRestrictions: false  
+    },
+
+    {
+        id: 3,
+        destination: "Bad Gastein",
+        country: "Österrike",
+        type: "Skidsemester",
+        grade: 4,
+        covidRestrictions: true  
+    },
+
+    {
+        id:4,
+        destination: "Barcelona",
+        country: "Spanien",
+        type: "Cityresa",
+        grade: 5,
+        covidRestrictions: true  
+    },
+
+    {
+        id: 5,
+        destination: "Santorini",
+        country: "Grekland",
+        type: "Solsemester",
+        grade: 4,
+        covidRestrictions: false  
+    },
+
+    {
+        id: 6, 
+        destination: "Prag",
+        country: "Tjeckien",
+        type: "Cityresa",
+        grade: 3,
+        covidRestrictions: false
+    },
+
+    {
+        id: 7,
+        destination: "Stockholm",
+        country: "Sverige",
+        type: "Cityresa",
+        grade: 2,
+        covidRestrictions: true  
+    },
+
+    {
+        id: 8, 
+        destination: "Kreta",
+        country: "Grekland",
+        type: "Solsemester",
+        grade: 3,
+        covidRestrictions: false  
+    },
+
+    {
+        id: 9,
+        destination: "Trysil",
+        country: "Norway",
+        type: "Skidsemester",
+        grade: 3,
+        covidRestrictions: true  
+    },
+
+    {
+        id: 10,
+        destination: "Venedig",
+        country: "Italien",
+        type: "Cityresa",
+        grade: 5,
+        covidRestrictions: true  
+    }
+
+
+
+
+
+]
